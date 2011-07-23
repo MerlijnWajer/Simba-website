404705d689fcdd1ef057477078dbc6ee8c632541
Merlijn Wajer <merlijn@wizzup.org>
2011-07-23 18:19:54 +0200

    Add sidebar + logo.

--- conf.py
+++ conf.py
@@ -96,7 +96,7 @@ html_theme = 'nature'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'nosidebar' : True}
+html_theme_options = {'nosidebar' : False}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -110,7 +110,7 @@ html_title = 'Project Simba Homepage'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'img/logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -184,7 +184,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+#latex_logo = 'img/logo.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
